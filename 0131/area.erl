743f8b9c4a52c9303acad6f0930e4bad2be796bd
Kwag De Gi <code@skyaperture.com>
2015-01-31 09:31:30 +0900

    debugged

--- 0131/area.erl
+++ 0131/area.erl
@@ -10,7 +10,7 @@ area({circle, Radius}) ->
 
 area({triangle, A, B, C}) ->
 	S = (A + B + C) / 2,
-	math:sqrt(S*(S-A)*(S-B)*(S-C));;
+	math:sqrt(S*(S-A)*(S-B)*(S-C));
 area(Other) ->
 	{invalid_object, Other}.
 
